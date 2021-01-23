a21ad7de7f201c38f38864229b0efa92908c918b
Brandon Olivier <brandon@brandonolivier.com>
2021-01-22 19:05:29 -0600

    Really get mutations working

--- src/main/app/mutations.clj
+++ src/main/app/mutations.clj
@@ -1,14 +1,16 @@
 (ns app.mutations
   (:require [app.resolvers :refer [list-table]]
             [com.wsscode.pathom.connect :as pc]
+            [mount.core :refer [defstate]]
             [taoensso.timbre :as log]))
 
 (pc/defmutation delete-person [env {list-id :list/id
                                     person-id :person/id}]
   {::pc/sym `delete-person}
   (log/info "Deleting person" person-id "from list" list-id)
-  (swap! list-table update list-id update :list-people (fn [old-list]
+  (swap! list-table update list-id update :list/people (fn [old-list]
                                                          (filterv #(not= person-id %)
                                                                   old-list))))
 
-(def mutations [delete-person])
+(defstate mutations
+  :start [delete-person])
